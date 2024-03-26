218dea338c8fcace993b3c89f76025eef3889035
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-03-26 11:31:14 -0500

    funcion id(direccion o posicion de memoria de variable) y asignacion de variables

--- L E S S O N - 1/pythonProject/main.py
+++ L E S S O N - 1/pythonProject/main.py
@@ -1,1 +1,38 @@
+"""
+miVariable = 2 #"Hola desde python"
+print(miVariable)
+print(miVariable)
+print(miVariable)
 
+miVariable = 10
+print(miVariable)
+"""
+"""
+x = 10
+y = 2
+z = x + y
+print(x)
+print(y)
+print(z)
+#direccion de literales o posicion de memoria donde se encuentra almacenada una variable
+#ya que es una memoria volatil
+print(id(x))
+print(id(y))
+print(id(z))
+
+## vd10. Direccion de Memoria y Variables en Python
+"""
+
+### E J E R C I C I O
+
+# declaracion de variables
+# Anderson Mancilla
+# 42662456
+# jsadnf@gmail.com
+nombre = 'Anderson Mancilla'
+numero = 42662456
+correo = 'jsadnf@gmail.com'
+
+print(nombre)
+print(numero)
+print(correo)

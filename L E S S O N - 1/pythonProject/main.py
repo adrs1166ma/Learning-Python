c0605bb9307fb8629b1fd8382f95e1394a3e356e
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-03-26 10:40:32 -0500

    base

--- L E S S O N - 1/pythonProject/main.py
+++ L E S S O N - 1/pythonProject/main.py
@@ -1,11 +1,1 @@
-n1 = int(input('Proporciona el Numero 1: '))
-n2 = int(input('Proporciona el Numero 2: '))
 
-if n1 > n2:
-    print('Numero 1 es mayor')
-else:
-<<<<<<< HEAD
-    print('Numero 2 es mayor')
-=======
-    print('Numero 2 es mayor')
->>>>>>> c49bf4b3ca4be468ca302bd3878e7831a3c3b9d1

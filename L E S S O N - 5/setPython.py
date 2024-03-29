dbe60423b4fd0fcba0cba873c7fbf3d6cdff4ca0
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-03-29 18:58:11 -0500

    c

---
+++ L E S S O N - 5/setPython.py
@@ -0,0 +1,34 @@
+# imprime aleatoriamente y no sepueden modificar pero si agregar
+# set
+planetas = {'Marte', 'Júpiter', 'Venus'}
+print(planetas)
+
+#largo
+print(len(planetas))
+
+# revisar si un elemento está presente
+print('Marte' in planetas)
+
+# agregar un elemento
+planetas.add('Tierra')
+print( planetas)
+
+#no se pueden duplicar elementos
+planetas.add('Tierra')
+print(planetas)
+
+# eliminar elemento posiblemente arrojando un error
+planetas.remove('Tierra')
+print(planetas)
+
+# eliminar elemento sin arrojar error
+planetas.discard('Júpiters')
+print(planetas)
+
+# limpiar set
+planetas.clear()
+print(planetas)
+
+# eliminar el set
+del planetas
+print(planetas)

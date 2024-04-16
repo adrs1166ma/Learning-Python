a1eb17ddd2fc5930e777a8ccb0d46b0df2cbe222
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-04-16 13:29:23 -0500

    agrego comentario

--- Ls2-Operadores/02-02-01-EjercicioRectangulo-UP.py
+++ Ls2-Operadores/02-02-01-EjercicioRectangulo-UP.py
@@ -1,14 +1,16 @@
 """
 Instrucciones de la tarea:
-- En el siguiente ejercicio se solicita calcular el área y el perímetro de un Rectángulo,
-para ello deberemos crear las siguientes variables:
+- En el siguiente ejercicio se solicita calcular
+  el área y el perímetro de un Rectángulo,
+  para ello deberemos crear las siguientes variables:
 
 alto (int)
 ancho (int)
 
 - El usuario debe proporcionar los valores de alto y ancho,
 y después se debe imprimir el resultado en el siguiente formato
-(no usar acentos y respetar los espacios, mayúsculas, minúsculas y saltos de línea):
+(no usar acentos y respetar los espacios, mayúsculas, minúsculas
+y saltos de línea):
 
 Proporciona el alto:
 Proporciona el ancho:
--- POO/01-Clases_Y_Objetos/01-03-00-CreacionObjetosConArgumentos-UP.py
+++ POO/01-Clases_Y_Objetos/01-03-00-CreacionObjetosConArgumentos-UP.py
@@ -4,7 +4,7 @@ class Persona:
         self.apellido = apellido
         self.edad = edad
 
-persona1 = Persona('Juan', 'Perez', 28)
+persona1 = Persona('Juan', 'Perez', 28) # crear obejo con su constructor
 print(persona1.nombre)
 print(persona1.apellido)
 print(persona1.edad)
